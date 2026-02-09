be9d8e3fe9689b897fbd8f7e17bd9c930021b526
Suresh <suresh.s@turbostart.co>
2026-02-09 16:29:59 +0530

    Add About and Technology components with respective content and styling
    - Created About component with a description of Easy Lane and an image.
    - Developed Technology component showcasing various tech features with icons and descriptions.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,18 @@
 import Navbar from "./components/layout/Navbar";
 import Hero from "./components/hero/page";
+import About from "./components/about/page";
+import Technology from "./components/technology/page";
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
 
       <Hero />
+        <div className="bg-white flex justify-center items-center section-width">
+        <img src="/admin-img.png" alt="Easy Lane" className="w-full sm:w-[80%] h-auto" />
+      </div>
+      <About />
+      <Technology />
 
     </div>
   );
