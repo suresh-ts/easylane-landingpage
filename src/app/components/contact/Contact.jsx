8611cb07e705a9ccf47f17fb9ac9d98e60642af5
Naresh R <naresh.r@turbostart.co>
2026-02-11 12:59:35 +0530

    email service form submission added

--- src/app/components/contact/Contact.jsx
+++ src/app/components/contact/Contact.jsx
@@ -13,12 +13,9 @@ const Contact = () => {
   const [countryError, setCountryError] = useState("");
   const [selectedCountry, setSelectedCountry] = useState("");
   const [selectedDialCode, setSelectedDialCode] = useState("");
-  const SERVICE_ID =
-    process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || "";
-  const TEMPLATE_ID =
-    process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || "template_d6mr98n";
-  const PUBLIC_KEY =
-    process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY || "yEbhfrTy5Jltra8Br";
+  const SERVICE_ID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || "";
+  const TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || "";
+  const PUBLIC_KEY = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY || "";
 
   useEffect(() => {
     let isMounted = true;
