9098568ddc9cb0acf04bc2b6b2c2fd709d5a2c57
Naresh R <naresh.r@turbostart.co>
2026-02-12 15:18:00 +0530

    meta description added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,12 +2,19 @@ import "./globals.css";
 
 export const metadata = {
   title: "Easy Lane - Fleet Optimization",
-  description: "",
+  description:
+    "Easy Lane empowers fleet owners, promotes entrepreneurship, and optimizes logistics across India with tech-driven solutions.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="dD5UO5kDigztvM2NgXBSC-JyvLqBZFdyAJ64sJJVcVE"
+        />
+      </head>
       <body
       >
         {children}
